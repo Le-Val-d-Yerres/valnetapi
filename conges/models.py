16e40494399c7852f9cb4fc14cc046be24f06cdf
rtrjl <rt@zone42.fr>
2016-02-10 16:54:20 +0100

    ajout modele

--- conges/models.py
+++ conges/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class TypeConge(models.Model):
+    nom = models.CharField()
+    ordre_presentation = models.IntegerField()
+
+class Conge(models.Model):
+    debut = models.DateField()
+    fin = models.DateField()
+    nb_jours = models.FloatField()
+    type_conge = models.ForeignKey(TypeConge)
